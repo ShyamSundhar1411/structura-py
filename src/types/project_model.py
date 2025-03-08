7b59a9d60208006a03cadcba541a262330cfb8d7
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-03-08 22:12:25 +0530

    feat: added project and architecture types

---
+++ src/types/project_model.py
@@ -0,0 +1,23 @@
+import os
+from typing import Literal
+
+from pydantic import BaseModel, Field, field_validator
+
+
+class ProjectModel(BaseModel):
+    name: str = Field(..., min_length=3, max_length=50, description="Project Name")
+    path: str = Field(..., min_length=3, max_length=50, description="Project Path")
+    description: str = Field(
+        ..., min_length=3, max_length=50, description="Project Description"
+    )
+    architecture: Literal["MVC", "MVC-API", "MVCS", "Hexagonal"] = Field(
+        ..., description="Project Architecture"
+    )
+
+    @field_validator("path")
+    def validate_path(cls, value):
+        """Ensure the path is valid (absolute or relative) and create if needed."""
+        abs_path = os.path.abspath(value)
+        if not os.path.isdir(abs_path):
+            raise ValueError("Path must be a directory, not a file.")
+        return abs_path
