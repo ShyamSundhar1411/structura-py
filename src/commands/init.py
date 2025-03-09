cc9c6dfd0fc345240176051dbea107a5122a3f3b
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-03-09 22:29:28 +0530

    feat: added initial prompt builder

--- src/commands/init.py
+++ src/commands/init.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 import typer
+from inquirer import prompt
 from pydantic import ValidationError
 
-from src.models.project_model import ProjectModel
 from src.utils.prompt_utils import input_prompt, select_prompt
 
 init_app = typer.Typer()
@@ -11,28 +11,33 @@ init_app = typer.Typer()
 
 @init_app.command()
 def init():
-    project_name = input_prompt(
-        field="project_name", message="Project Name", default="my-project"
+    prompt_data = []
+    prompt_data.append(
+        input_prompt(field="project_name", message="Project Name", default="my-project")
     )
-    project_path = input_prompt(
-        field="project_path", message="Project Path", default=str(Path.cwd())
-    )
-    project_description = input_prompt(
-        field="project_description", message="A new python project", default=""
-    )
-    project_architecture = select_prompt(
-        field="project_architecture",
-        message="Project Architecture",
-        choices=["MVC", "MVC-API", "MVCS", "Hexagonal"],
-    )
-    try:
-        project_data = ProjectModel(
-            name=project_name,
-            path=project_path,
-            description=project_description,
-            architecture=project_architecture,
+    prompt_data.append(
+        input_prompt(
+            field="project_path", message="Project Path", default=str(Path.cwd())
         )
-        typer.echo(f"✅ Project initialized:\n{project_data.model_dump_json(indent=2)}")
+    )
+    prompt_data.append(
+        input_prompt(
+            field="project_description",
+            message="Project Description",
+            default="A new python project",
+        )
+    )
+    prompt_data.append(
+        select_prompt(
+            field="project_architecture",
+            message="Project Architecture",
+            choices=["MVC", "MVC-API", "MVCS", "Hexagonal"],
+        )
+    )
+    _ = prompt(prompt_data)
+    try:
+        typer.echo("🚀 Initializing project...")
+        typer.echo("✅ Project initialized:")
     except ValidationError as e:
         typer.echo("❌ Error: Invalid project data")
         typer.echo(e)
